1d681b68e66483b83a3ed3c7f38c0ab4ade0e297
borisinparis <mnkymgaa@gmail>
2025-03-12 17:25:35 +0800

    23232

--- src/middlewares/login/validateLoginMiddleWares.js
+++ src/middlewares/login/validateLoginMiddleWares.js
@@ -1,6 +1,6 @@
 import { Users } from "../../models/users.model.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
 export const validateLogin = async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -10,7 +10,31 @@ export const validateLogin = async (req, res, next) => {
     if (userFound) {
       if (password) {
         const isMatch = await bcrypt.compare(password, userFound.password);
+        const dateAsMiloSecond = Date.now();
+        console.log(dateAsMiloSecond);
+        const currentDate = new Date(dateAsMiloSecond).toLocaleString();
+
+        const dateAsSecond = Math.floor(dateAsMiloSecond / 1000);
+        console.log(dateAsMiloSecond);
+        const currentDateInSeconds = new Date(
+          dateAsSecond * 1000
+        ).toLocaleString();
+
+        const expirationTime = dateAsSecond + 60 * 60;
+        const expirationDate = new Date(expirationTime * 1000).toLocaleString();
+        const decodePassword = "123";
         if (isMatch) {
+          const token = jwt.sign(
+            {
+              exp: Math.floor(Date.now() / 1000) + 60 * 60,
+              data: userFound,
+            },
+            decodePassword
+          );
+          console.log("this is token", token);
+          const decoded = jwt.verify(token, "123");
+          console.log("this is decoded", token);
+
           return res.status(200).json({
             success: true,
             user: userFound,
